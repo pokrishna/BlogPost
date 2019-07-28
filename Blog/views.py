b071953d165231959a3f3293cdada3dcb8d1ea2d
pokrishna <pokrishna@gmail.com>
2019-07-28 14:12:26 +0530

    second commit

--- Blog/views.py
+++ Blog/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
+# def home(request):
+#     context={'posts':Post.objects.all()}
+#     return render(request,'Blog/home.html',context)
+#
+# def about(request):
+#     return render(request,'Blog/about.html',{'title':'About Blog'})
+# def home(request):
+#     context={'posts':Post.objects.all()}
+#     return render(request,'Blog/home.html',context)
+#
+# def about(request):
+#     return render(request,'Blog/about.html',{'title':'About Blog'})
+
 
 def home(request):
     context={'posts':Post.objects.all()}
